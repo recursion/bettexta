c20335cd67f17ac5dc7ca4bf1cd646801391236d
Michael Symmes <msymmes@gmail.com>
2019-09-05 09:34:28 -0400

    Added Mutation Observer.

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -6,13 +6,30 @@
   import CodeMirror from "../CodeMirror";
   import ControlButton from "./ControlButton.svelte";
   import Settings from "../settings";
+  const observerConfig = { attributes: true, childList: true, subtree: true };
 
   let cm;
+  let bottom;
   let active = false;
   let settings = Settings(id, (key, value) => {
     console.log("Setting key: ", key, " to value: ", value);
     cm.setOption(key, value);
   });
+
+  // setup a mutation observer to track element size changes
+  function handleTAMutation(changes, observer) {
+    const size = target.getBoundingClientRect();
+    bottom = size.bottom;
+  }
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(handleTAMutation);
+
+  // Start observing the target node for configured mutations
+  observer.observe(target, observerConfig);
+
+  const size = target.getBoundingClientRect();
+  bottom = size.bottom;
 
   function toggleActive() {
     active = !active;
@@ -24,9 +41,32 @@
 
   $: if (active) {
     if (!cm) {
+      const og_size = target.getBoundingClientRect();
       cm = CodeMirror.fromTextArea(target, basic);
+
       const codeMirror = document.querySelector(".CodeMirror");
-      codeMirror.classList.add("bta_" + id);
+      const uid = "bta_" + id;
+
+      codeMirror.classList.add(uid);
+      codeMirror.style.height = og_size.height;
+      codeMirror.style.width = og_size.width;
+
+      const size = codeMirror.getBoundingClientRect();
+      bottom = size.bottom;
+
+      // setup a mutation observer to track element size changes
+      function handleMutation(changes, observer) {
+        const target = getThisCodeMirrorEl();
+        const size = target.getBoundingClientRect();
+        bottom = size.bottom;
+      }
+
+      // Create an observer instance linked to the callback function
+      const observer = new MutationObserver(handleMutation);
+
+      // Start observing the target node for configured mutations
+      observer.observe(codeMirror, observerConfig);
+
       // apply current settings
       Object.keys($settings).forEach(k => {
         cm.setOption(k, $settings[k]);
@@ -36,6 +76,8 @@
     if (cm) {
       try {
         cm.toTextArea();
+        const size = target.getBoundingClientRect();
+        bottom = size.bottom;
       } catch (e) {
         console.log("--> ", e);
       }
@@ -48,4 +90,15 @@
   }
 </script>
 
-<ControlButton {updateSetting} {target} {id} {toggleActive} {active} />
+<style>
+  :global(.CodeMirror) {
+    resize: both;
+    overflow: auto !important;
+    scrollbar-width: 0;
+  }
+  :global(.CodeMirror::-webkit-scrollbar) {
+    width: 0;
+  }
+</style>
+
+<ControlButton {updateSetting} {target} {id} {toggleActive} {active} {bottom} />
